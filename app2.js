64df1b3ddd9b4ba37b58646a4ce375fbe1475cb7
yuu <heinhtun1994@gmail.com>
2023-05-06 16:07:13 +0630

    commit 3

---
+++ app2.js
@@ -0,0 +1,12 @@
+const app2 = Vue.createApp({
+    data(){
+        return {
+            name : "Aung Aung",
+            age : 21 ,
+            job : "student",
+            enroll : true ,
+        }
+    }
+});
+
+app2.mount('#app2');
